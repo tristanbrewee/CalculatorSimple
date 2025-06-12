b49f4812b034beb9afd495ed768bde98ab14bfa9
tristan <tristanbrewee@gmail.com>
2025-06-12 16:21:16 +0200

    - Added a basic layout to the JFrame - Added a TextArea to display the input and output - Added the buttons for when the mouse is used to enter input

--- src/com/breweetristan/swing/MainWindow.java
+++ src/com/breweetristan/swing/MainWindow.java
@@ -1,6 +1,7 @@
 package com.breweetristan.swing;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow {
 
@@ -20,9 +21,76 @@ public class MainWindow {
         window = new JFrame();
         window.setTitle("Simple calculator");
         window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        window.setSize(800, 500);
+        window.setSize(450, 500);
         window.setResizable(false);
         window.setLocationRelativeTo(null); //This centers the JFrame
+
+        addLayouts();
+    }
+
+    /*
+    Add the Layouts to the JFrame
+     */
+    private void addLayouts(){
+        /*
+        Add the TextArea to the top center
+         */
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 15, 15));
+
+        TextArea textArea = new TextArea();
+        textArea.setEditable(false);
+        topPanel.add(textArea);
+
+        window.add(topPanel, BorderLayout.NORTH);
+
+        /*
+        Add the buttons to the center
+         */
+        JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new GridLayout(4, 4, 5, 5));
+
+        addButtons(centerPanel);
+
+        window.add(centerPanel, BorderLayout.CENTER);
+    }
+    /*
+    Add the JButtons to the centerPanel
+     */
+    private void addButtons(JPanel centerPanel){
+        JButton btnOne = new JButton("1");
+        JButton btnTwo = new JButton("2");
+        JButton btnThree = new JButton("3");
+        JButton btnFour = new JButton("4");
+        JButton btnFive = new JButton("5");
+        JButton btnSix = new JButton("6");
+        JButton btnSeven = new JButton("7");
+        JButton btnEight = new JButton("8");
+        JButton btnNine = new JButton("9");
+        JButton btnZero = new JButton("0");
+        JButton btnEquals = new JButton("=");
+        JButton btnDot = new JButton(".");
+        JButton btnAdd = new JButton("+");
+        JButton btnSubtract = new JButton("-");
+        JButton btnMultiply = new JButton("*");
+        JButton btnDivide = new JButton("/");
+
+        centerPanel.add(btnOne);
+        centerPanel.add(btnTwo);
+        centerPanel.add(btnThree);
+        centerPanel.add(btnAdd);
+        centerPanel.add(btnFour);
+        centerPanel.add(btnFive);
+        centerPanel.add(btnSix);
+        centerPanel.add(btnSubtract);
+        centerPanel.add(btnSeven);
+        centerPanel.add(btnEight);
+        centerPanel.add(btnNine);
+        centerPanel.add(btnMultiply);
+        centerPanel.add(btnDot);
+        centerPanel.add(btnZero);
+        centerPanel.add(btnEquals);
+        centerPanel.add(btnDivide);
     }
 
     /*
