98d8feb1faebf9201d04c33d9cc028aa09e4d134
Tristan <tristanbrewee@gmail.com>
2025-06-15 17:31:40 +0200

    - Edited the error-message when parsing to double fails. - Added CalculationClass.solve() - Added CalculationClass.findAndSolve() - These two methods together with CalculationClass.calculate() now solve every given calculation.

--- src/com/breweetristan/calculations/CalculationClass.java
+++ src/com/breweetristan/calculations/CalculationClass.java
@@ -12,7 +12,89 @@ public class CalculationClass {
         String output = IOClass.checkInput(calculation);
         if (!output.equals(calculation))
             return output;
+        output = solve(calculation);
         return output;
+    }
+
+    /*
+    This method solves the given calculation based on priority operations
+     */
+    private static String solve(String calculation){
+        int amountPriorityOperations = getNumberOfPriorityOperations(calculation);
+        if (amountPriorityOperations > 0)
+            calculation = findAndSolve(calculation, true);
+        return findAndSolve(calculation, false);
+    }
+
+    private static String findAndSolve(String calculation, boolean priority){
+        //Determine which operations we're solving
+        char operation1, operation2;
+        if (priority){
+            operation1 = '*';
+            operation2 = '/';
+        }else {
+            operation1 = '+';
+            operation2 = '-';
+        }
+        //Loop over calculation
+        for (int i = 1; i < calculation.length(); i++){
+            //Operation found
+            if (calculation.charAt(i) == operation1 ||
+                    calculation.charAt(i) == operation2){
+                String number1, number2;
+                int indexStart = 0, indexEnd = calculation.length();
+                //Determine the start index of the number before the operation
+                for(int j = i - 1; j >= 0; j--){
+                    if (calculation.charAt(j) == '/' ||
+                            calculation.charAt(j) == '*' ||
+                            calculation.charAt(j) == '+' ){
+                        indexStart = j + 1;
+                        break;
+                    } else if (calculation.charAt(j) == '-') {
+                        if (j == 0 ||
+                                calculation.charAt(j - 1) == '/' ||
+                                calculation.charAt(j - 1) == '*' ||
+                                calculation.charAt(j - 1) == '-' ||
+                                calculation.charAt(j - 1) == '+'){
+                            indexStart = j;
+                            break;
+                        }
+                    }
+                }
+                //Determine the end index of the number after the operation
+                for(int j = i + 1; j <= calculation.length(); j++){
+                    if (j == calculation.length() ||
+                            calculation.charAt(j) == '/' ||
+                            calculation.charAt(j) == '*' ||
+                            calculation.charAt(j) == '+'){
+                        indexEnd = j;
+                        break;
+                    }
+                    if (calculation.charAt(j) == '-')
+                        if (j != i + 1){
+                            indexEnd = j;
+                            break;
+                        }
+                }
+                //Calculate the found operation on the found 2 numbers
+                number1 = calculation.substring(indexStart, i);
+                number2 = calculation.substring(i + 1, indexEnd);
+                String result = calculate(number1, number2, calculation.charAt(i));
+                //Replace the solved part of the calculation and reset the loop
+                calculation = calculation.substring(0, indexStart) + result + calculation.substring(indexEnd);
+                System.out.println(calculation);
+                i = 0;
+            }
+        }
+        return calculation;
+    }
+
+    /*
+    This methods gets the amount of priority operations from the given calculation
+     */
+    private static int getNumberOfPriorityOperations(String calculation){
+        return (int)calculation.chars().filter(e->e=='/').count() +
+                (int)calculation.chars().filter(e->e =='*').count();
     }
 
     /*
@@ -20,13 +102,13 @@ public class CalculationClass {
     It catches possible incorrect input which leads to number1 or number2 not being a number
     It catches possible incorrect input which leads to 'operation' not being a valid operation
      */
-    public static String calculate(String number1, String number2, char operation){
+    private static String calculate(String number1, String number2, char operation){
         double numberA, numberB;
         try{
             numberA = Double.parseDouble(number1);
             numberB = Double.parseDouble(number2);
         }catch (Exception e){
-            return e.getMessage();
+            return "Error parsing numbers";
         }
         try{
             if (operation == '+')
